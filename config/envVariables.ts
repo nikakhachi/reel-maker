a14b40735497b9fb6cafd81ab7ae7228d82b2e74
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-15 00:23:06 +0400

    add aws variables to ENV_VARIABLES

--- config/envVariables.ts
+++ config/envVariables.ts
@@ -7,6 +7,10 @@ const ENV_VARIABLES = [
   "DATABASE_URL",
   "ASSEMBLY_API_KEY",
   "RAPID_API_KEY",
+  "AWS_ACCESS_KEY_ID",
+  "AWS_SECRET_ACCESS_KEY_ID",
+  "AWS_REGION",
+  "AWS_S3_BUCKET_NAME",
 ];
 const NODE_ENV_OPTIONS = ["development", "production", "testing"];
 
