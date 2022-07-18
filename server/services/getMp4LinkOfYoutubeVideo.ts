941ce6174f2df68f65b1dd7716aae10b41b0cd38
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-18 20:42:34 +0400

    check for other formats if 720p doesnt exist for video

--- server/services/getMp4LinkOfYoutubeVideo.ts
+++ server/services/getMp4LinkOfYoutubeVideo.ts
@@ -1,6 +1,22 @@
 import axios from "axios";
 import logger from "../utils/logger";
 
+type YoutubeVideoFormat = "720p" | "480p" | "360p" | "240p" | "144p";
+
+const getVideoUrlByFormat = (data: any, format: YoutubeVideoFormat) =>
+  (Object.values(data.link) as string[][]).find((item) => item[3] === "720p" && item[4].includes("video/mp4"))?.[0] as string;
+
+const getVideoUrl = (data: any) => {
+  const formats = ["720p", "480p", "360p", "240p", "144p"];
+  let videoUrl: string | undefined;
+  for (const format of formats) {
+    logger.info(`Getting ${format} format`);
+    videoUrl = getVideoUrlByFormat(data, format as YoutubeVideoFormat);
+    if (videoUrl) break;
+  }
+  return videoUrl || null;
+};
+
 export const getMP4LinkOfYoutubeVideo = (videoId: string): Promise<string> =>
   new Promise((res, rej) => {
     logger.info(`Getting MP4 Url for Video : ${videoId}`);
@@ -18,14 +34,10 @@ export const getMP4LinkOfYoutubeVideo = (videoId: string): Promise<string> =>
       .request(options)
       .then(function ({ data }) {
         if (data.status === "ok") {
-          const adaptive480p = (Object.values(data.link) as string[][]).find(
-            (item) => item[3] === "720p" && item[4].includes("video/mp4")
-          )?.[0] as string;
-          if (!adaptive480p) {
-            rej("Cant find 720 video/mp4 link");
-          }
-          logger.info(adaptive480p);
-          res(adaptive480p);
+          let videoUrl = getVideoUrl(data);
+          if (!videoUrl) return rej("Video format is broken");
+          logger.info(videoUrl);
+          res(videoUrl);
         } else {
           rej(data);
         }
