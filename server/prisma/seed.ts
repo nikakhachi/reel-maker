2e9d6ba4fc9a2c39b79dfc2b54d44e6984af83ae
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-19 12:51:59 +0400

    remove commented code in seed

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -21,41 +21,4 @@ const prisma = new PrismaClient();
     ],
   });
   logger.info(`Youtube Video Status seeded successfully.`);
-  // await prisma.subscription.createMany({
-  //   data: [
-  //     {
-  //       id: 1,
-  //       title: "Free",
-  //       durationInDays: 10,
-  //       priceInDollars: 0,
-  //       transcriptionSeconds: 60 * 10,
-  //       isActive: true,
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Basic",
-  //       durationInDays: 30,
-  //       priceInDollars: 20,
-  //       transcriptionSeconds: 60 * 30,
-  //       isActive: true,
-  //     },
-  //     {
-  //       id: 3,
-  //       title: "Premium",
-  //       durationInDays: 30,
-  //       priceInDollars: 50,
-  //       transcriptionSeconds: 60 * 60 * 2,
-  //       isActive: true,
-  //     },
-  //     {
-  //       id: 4,
-  //       title: "Enterprise",
-  //       durationInDays: 30,
-  //       priceInDollars: 200,
-  //       transcriptionSeconds: 60 * 60 * 10,
-  //       isActive: true,
-  //     },
-  //   ],
-  // });
-  // logger.info(`Subscription seeded successfully.`);
 })();
