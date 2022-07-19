a356d3512062f67a0f99bc5f4519e398cc62aced
Nika Khachiashvili <n.khachiashvili1@gmail.com>
2022-07-19 19:16:22 +0400

    remove xml-stream package and temporarily disable s3 zip download

--- server/aws/index.ts
+++ server/aws/index.ts
@@ -1,7 +1,7 @@
 import { config, S3 } from "aws-sdk";
 import { Response } from "express";
-const s3Zip = require("s3-zip");
-const XmlStream = require("xml-stream");
+// const s3Zip = require("s3-zip");
+// const XmlStream = require("no-gyp-xml-stream");
 
 config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -12,23 +12,24 @@ config.update({
 const s3 = new S3();
 
 export const downloadS3FolderAsZip = (res: Response, folder: string) => {
-  const region = process.env.AWS_REGION;
-  const bucket = process.env.AWS_S3_BUCKET_NAME || "";
+  res.status(400).json("Under Construction");
+  // const region = process.env.AWS_REGION;
+  // const bucket = process.env.AWS_S3_BUCKET_NAME || "";
 
-  const params = {
-    Bucket: bucket,
-    Prefix: folder,
-  };
+  // const params = {
+  //   Bucket: bucket,
+  //   Prefix: folder,
+  // };
 
-  const filesArray: string[] = [];
-  const files = s3.listObjects(params).createReadStream();
-  const xml = new XmlStream(files);
-  xml.collect("Key");
-  xml.on("endElement: Key", (item: any) => {
-    filesArray.push(item["$text"].substr(folder.length));
-  });
+  // const filesArray: string[] = [];
+  // const files = s3.listObjects(params).createReadStream();
+  // const xml = new XmlStream(files);
+  // xml.collect("Key");
+  // xml.on("endElement: Key", (item: any) => {
+  //   filesArray.push(item["$text"].substr(folder.length));
+  // });
 
-  xml.on("end", () => {
-    s3Zip.archive({ region: region, bucket: bucket, preserveFolderStructure: true }, folder, filesArray).pipe(res);
-  });
+  // xml.on("end", () => {
+  //   s3Zip.archive({ region: region, bucket: bucket, preserveFolderStructure: true }, folder, filesArray).pipe(res);
+  // });
 };
